068cfc030bca6c2daaef877ed6b7940bd520eafc
Josue Emanuel Mosquera <josue.mosquera@valmar.com.pe>
2016-02-19 16:22:46 +0000

    Changes made by Josue Emanuel Mosquera (josue.mosquera@valmar.com.pe).

--- Animacion/js/app.js
+++ Animacion/js/app.js
@@ -23,8 +23,28 @@
 
 
 // ...additional event handlers here...
-/*alert( "Antes" );
-document.addEventListener('deviceready', function() {
+//console.log( "Antes" );
+//alert( "Evento" );
+/*document.addEventListener('deviceready', function() {
     alert( "Evento" );
     console.log('hola');
 }, false);*/
+var url='http://54.213.238.161/app/';
+
+function clase(num,tit){
+	var iframe='<iframe class="anima" src="'+url+num+'"> </iframe>';
+	//var iframe='hola';
+    $('#memo').val(iframe);
+    $('#titulo').html(tit);
+    $('#animacion').html(iframe);
+    //var nu = $('#memo').val();
+    //console.log(nu);
+    
+    window.location.href = "#drawer-starred";
+}
+
+function limpiar(){
+	$('#titulo').html('nada');
+	$('#animacion').html('nada');
+	window.location.href = "#drawer-home";
+}
